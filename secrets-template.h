6f2e2ffaf830e526bebc04ed11a77d817150f4fb
tobhae <tobhae.dev@pm.me>
2026-02-22 18:19:31 +0100

    Updated the secrets template

--- secrets-template.h
+++ secrets-template.h
@@ -1,16 +1,23 @@
 #pragma once
 
 /*
-This file is read by the node and needs to be renamed to: "secrets.h".
-Enter your credentials and make sure it is added to .gitignore.
+This project requires a local secrets.h file containing WiFi and InfluxDB credentials.
+
+You need to:
+1. Create a new file named "secrets.h" in the same folder.
+2. Copy the contents of this template into secrets.h.
+3. Fill in your own credentials.
+
+Note:
+The file "secrets.h" is ignored by Git and remains local.
 */
 
-// WiFi
+/* WiFi */
 #define WIFI_SSID "your_ssid"
 #define WIFI_PASSWORD "your_wifi_password"
 
-// InfluxDB
-#define INFLUX_HOST "ipv4_address"
-#define INFLUX_ORG "influxdb_org"
+/* InfluxDB */
+#define INFLUX_HOST "your_ipv4_address"
+#define INFLUX_ORG "influxdb_organization"
 #define INFLUX_BUCKET "influxdb_bucket"
-#define INFLUX_TOKEN "your_influxdb_token"
+#define INFLUX_TOKEN "influxdb_token"
