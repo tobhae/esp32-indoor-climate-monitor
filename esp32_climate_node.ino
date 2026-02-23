571c75bcc50d80c646ee5c78d1a0bc53c0c229b9
tobhae <tobhae.dev@pm.me>
2026-02-23 17:29:29 +0100

    Refactoring done for the whole program, certain logic now have their own functions

--- esp32_climate_node.ino
+++ esp32_climate_node.ino
@@ -8,30 +8,32 @@
 
 #define SEALEVELPRESSURE_HPA (1013.25)
 #define uS_TO_S_FACTOR 1000000ULL
-#define TIME_TO_SLEEP (15) /* Low value for testing*/
+#define TIME_TO_SLEEP (15) /* Low value for testing, change to 30 minutes (30 * 60) when deploying. */
 
-// Wifi config
+/* WiFi credentials (defined in secrets.h) */
 const char* ssid = WIFI_SSID;
 const char* password = WIFI_PASSWORD;
 
-// InfluxDB config
+/* InfluxDB connection parameters (defined in secrets.h) */
 const char* influx_host = INFLUX_HOST;
 const char* influx_org = INFLUX_ORG;
 const char* influx_bucket = INFLUX_BUCKET;
 const char* influx_token = INFLUX_TOKEN;
 
-char influx_url[256];
+/* Represents a single measurement sample */
+struct ClimateData {
+  float temperature;  // °C
+  float humidity;     // %
+  float pressure;     // hPa
+};
 
-Adafruit_BME280 bme;
+/* Global runtime objets */
+char influx_url[256]; // Buffer for fully constructed InfluxDB write endpoint URL
+Adafruit_BME280 bme;  // BME280 sensor instance (I2C)
 
 /*
 TODO [Architecture]:
-- Refactor WiFi and HTTP logic into dedicated functions to separate networking from sensor logic.
-- Move InfluxDB Line Protocol construction into its own function to simplify loop() and improve readability.
 - Remove Serial.print's that might be redudant, and maybe even replace some with a compile-time DEBUG flag.
-
-TODO [Power]:
-- Change sampling interval from every 10 seconds to every 30 minutes when deploying.
 
 TODO [Portability]:
 - Consider moving location (InfluxDB Line Protocol) to a configurable constant or secrets/config file for easier deployment of multiple nodes.
@@ -42,6 +44,43 @@ TODO [Portability]:
 */
 
 void setup() {
+  init_hardware();
+  build_influxdb_url();
+
+  ClimateData data = read_climate();
+
+  if(!connect_wifi()) {
+    esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+    esp_deep_sleep_start();
+  }
+
+  char payload[128];
+
+  if (build_influx_payload(payload, sizeof(payload), data)) {
+
+    if (post_influxdb(payload, strlen(payload))) {
+      Serial.println("InfluxDB write successful.");
+    } else {
+      Serial.println("InfluxDB write failed.");
+    }
+  }
+
+  /* Shutdown WiFi */
+  WiFi.disconnect(true);
+  WiFi.mode(WIFI_OFF);
+
+  /* After successful transmission, enter deep sleep. */
+  Serial.println("Transmission finished, going to sleep.");
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  esp_deep_sleep_start();
+}
+
+void loop() {
+  /* Function unused. Execution cycle occurs entirely in setup() and enters deep sleep when finished. */
+}
+
+void init_hardware() {
+  /* Initializes serial communication, I2C bus, and BME280 sensor. Halts execution if sensor initialization fails. */
   Serial.begin(115200);
   delay(1000);
 
@@ -54,31 +93,10 @@ void setup() {
   }
 
   Serial.println("BME280 ready.");
-
-  connect_wifi();
-
-  /* Initialize metrics */
-  float temperature = bme.readTemperature();
-  float humidity = bme.readHumidity();
-  float pressure = bme.readPressure() / 100.0F;
-
-  /* Build URL */
-  snprintf(
-    influx_url, sizeof(influx_url), 
-    "%s/api/v2/write?org=%s&bucket=%s&precision=s", 
-    influx_host, influx_org, influx_bucket);
-
-  post_influxdb(temperature, humidity, pressure);
-
-  WiFi.disconnect(true);
-  WiFi.mode(WIFI_OFF);
-
-  Serial.println("Transmission finished, going to sleep.");
-  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
-  esp_deep_sleep_start();
 }
 
-void connect_wifi() {
+bool connect_wifi() {
+  /* Attempts to connect to configured WiFi network (configured in secrets.h). Returns true if connection succeeds, otherwise false. */
   WiFi.begin(ssid, password);
 
   unsigned long start = millis();
@@ -88,23 +106,37 @@ void connect_wifi() {
     delay(500);
   }
 
-  if (WiFi.status() != WL_CONNECTED) {
-    Serial.println("WiFi failed, sleeping anyway.");
-    esp_deep_sleep_start();
-  } else {
+  if (WiFi.status() == WL_CONNECTED) {
     Serial.println("Connected to WiFi.");
+    return true;
   }
+
+  Serial.println("WiFi connection failed.");
+  return false;
 }
 
-void loop() {
-  /* Function unused. Execution cycle occurs entirely in setup() and enters deep sleep when finished. */
+ClimateData read_climate() {
+  /* Reads temperature, humidity, and pressure from BME280. */
+  ClimateData data;
+  data.temperature = bme.readTemperature();
+  data.humidity = bme.readHumidity();
+  data.pressure = bme.readPressure() / 100.0F;
+
+  return data;
 }
 
-void post_influxdb(float temp, float hum, float pres) {
+void build_influxdb_url() {
+  /* Constructs the full InfluxDB write endpoint URL */
+  snprintf(influx_url, sizeof(influx_url), 
+           "%s/api/v2/write?org=%s&bucket=%s&precision=s",
+           influx_host, influx_org, influx_bucket);
+}
 
+bool post_influxdb(const char* payload, size_t len) {
+  /* Send provided Line Protocol payload to InfluxDB, returns true if HTTP response is successful (204). */
   if (WiFi.status() != WL_CONNECTED) {
-  Serial.println("WiFi not connected!");
-  WiFi.reconnect();
+    Serial.println("WiFi connection failed.");
+    return false;
   }
 
   HTTPClient http;
@@ -118,7 +150,17 @@ void post_influxdb(float temp, float hum, float pres) {
   http.addHeader("Authorization", auth_header);
   http.addHeader("Content-Type", "text/plain; charset=utf-8");
 
+  int httpResponseCode = http.POST((uint8_t*)payload, len);
+  http.end();
+
+  return (httpResponseCode == 204);
+}
+
+bool build_influx_payload(char* buffer, size_t size, const ClimateData& data) {
+
   /*
+  Formats ClimateData into InfluxDB Line Protocol.
+
   InfluxDB Line Protocol format:
 
   measurement, tag=value field1=value1, field2=value2, field3=value3
@@ -129,28 +171,9 @@ void post_influxdb(float temp, float hum, float pres) {
   - The fields corresponds to each metric collected, i.e. temperature, humidity, and pressure.
   */
 
-  char payload[128];
+  int len = snprintf(buffer, size, 
+  "indoor_climate,location=test temperature=%.2f,humidity=%.2f,pressure=%.2f",
+  data.temperature, data.humidity, data.pressure);
 
-  int len = snprintf(
-    payload, sizeof(payload), 
-    "indoor_climate,location=test temperature=%.2f,humidity=%.2f,pressure=%.2f",
-    temp, hum, pres
-  );
-
-  if (len < 0 || len >= sizeof(payload)) {
-    Serial.println("Payload truncated or error.");
-    http.end();
-    return;
-  }
-
-  int httpResponseCode = http.POST((uint8_t*)payload, len);
-
-  if (httpResponseCode == 204) {
-    Serial.println("InfluxDB write successful.");
-  } else {
-    Serial.print("InfluxDB write failed, code: ");
-    Serial.println(httpResponseCode);
-  }
-
-  http.end();
+  return (len > 0 && len < size);
 }
